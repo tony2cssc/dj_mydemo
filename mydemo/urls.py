04369efadd32057b1b9be210a9c53db1b12498e8
Jijing Guo <jijing.guo@gmail.com>
2014-10-10 17:46:38 +0800

    partially finished chapter 3

--- mydemo/urls.py
+++ mydemo/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from mydemo.views import hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mydemo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'mydemo.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^hello/$', hello),
+                       url(r'^time/$', current_datetime),
+                       url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+                       )
